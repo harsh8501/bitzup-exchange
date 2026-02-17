cedbae6877cd0537b0dc541e8bff5279d74c8e67
Harsh Kushwah <rizwankhan36200@gmail.com>
2026-02-17 15:40:52 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/theme.css';
 import './styles/layout.css';
 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+
+if (redirect) {
+  window.history.replaceState(null, "", "/bitzup-exchange" + redirect);
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter basename="/bitzup-exchange">
         <App />
